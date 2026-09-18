7ebc67bae0acc6afacac77b7d27548a51e0c9a74
AfreenAlim973 <afreenalim538@gmail.com>
2026-09-18 17:22:54 +0530

    Fix Gmail SMTP connection

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ require("dotenv").config();
 const app = express();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
